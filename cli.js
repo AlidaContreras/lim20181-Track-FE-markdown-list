47013580f68819a6602bfd8c7371dfb791ea1dc5
AlidaContreras <alidacontreras01@gmail.com>
2018-09-04 16:32:53 -0500

    Master OK

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-const readDoc = require('./lib/index')
+const mdLinks  = require('./lib/index')
 //Funcion para leer el contenido del documento MD
 const [, , ...direccion] = process.argv
 
-readDoc(direccion).then((res) => { 
+mdLinks(direccion).then((res) => { 
   console.log(res) 
 });
 
