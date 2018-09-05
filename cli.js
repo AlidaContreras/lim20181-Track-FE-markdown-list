d00219b34c2c1b517042128afc59ee0861cd516c
AlidaContreras <alidacontreras01@gmail.com>
2018-09-04 19:02:26 -0500

    falta probar la promesa

--- cli.js
+++ cli.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 const mdLinks  = require('./lib/index')
 //Funcion para leer el contenido del documento MD
-const [, , ...direccion] = process.argv
+const [, , ...direction] = process.argv
+// if(typeof direction[0] !== undefined){
+  
+// }
 
-mdLinks (direccion).then((res) => {
+console.log(mdLinks);
+
+mdLinks(direction[0]).then((res) => {
   console.log(res)
+
 });
 
 
-//npm install --save markdown-link-extractor
+//npm install --save markdown-link-extractor     https://github.com/tcort/markdown-link-extractor
 //npm install --save link-check
